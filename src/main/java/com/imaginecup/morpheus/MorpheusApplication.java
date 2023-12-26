dbb79108e12e866bd236d9de43b71dbf45f533cc
10Hyuna <kimha105@naver.com>
2023-12-27 01:36:40 +0900

    init

---
+++ src/main/java/com/imaginecup/morpheus/MorpheusApplication.java
@@ -0,0 +1,13 @@
+package com.imaginecup.morpheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorpheusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorpheusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imaginecup/morpheus/MorpheusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imaginecup.morpheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorpheusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
