ff62a1feb207b54f2f62852c82067df6da9fc764
10Hyuna <kimha105@naver.com>
2023-12-31 02:14:01 +0900

    fix: Java internal character class reference error

--- src/main/java/com/imaginecup/morpheus/character/dao/CharacterRepository.java
+++ src/main/java/com/imaginecup/morpheus/character/dao/CharacterRepository.java
@@ -1,5 +1,6 @@
 package com.imaginecup.morpheus.character.dao;
 
+import com.imaginecup.morpheus.character.domain.Character;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CharacterRepository extends JpaRepository<Character, Long> {
