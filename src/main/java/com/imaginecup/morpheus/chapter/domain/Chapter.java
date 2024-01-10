31397d65bd7c303072027082d96952d3f91ad725
10Hyuna <kimha105@naver.com>
2024-01-11 02:16:27 +0900

    feat: chapter entity

---
+++ src/main/java/com/imaginecup/morpheus/chapter/domain/Chapter.java
@@ -0,0 +1,42 @@
+package com.imaginecup.morpheus.chapter.domain;
+
+import com.imaginecup.morpheus.fairy.domain.Fairy;
+import com.imaginecup.morpheus.picture.domain.Picture;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+@Entity
+public class Chapter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String story;
+
+    @Column(nullable = false)
+    private String plot;
+
+    @Column(nullable = false)
+    private String background;
+
+    @Column(nullable = false)
+    private String narrativeText;
+
+    @OneToOne
+    @JoinColumn(name = "picture_id")
+    private Picture image;
+
+    @ManyToOne
+    @JoinColumn(name ="fairy_id", nullable = false)
+    private Fairy fairy;
+
+}
