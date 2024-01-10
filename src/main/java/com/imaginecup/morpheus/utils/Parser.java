7dee9f804a18df7311143c316388db169f591d29
10Hyuna <kimha105@naver.com>
2024-01-10 23:22:38 +0900

    fix: character prompt for error with letters in image

--- src/main/java/com/imaginecup/morpheus/utils/Parser.java
+++ src/main/java/com/imaginecup/morpheus/utils/Parser.java
@@ -12,7 +12,6 @@ public class Parser {
                 characterCreationForm.getAppearance(),
                 characterCreationForm.getPersonality());
 
-        System.out.println(prompt);
         return prompt;
     }
 
