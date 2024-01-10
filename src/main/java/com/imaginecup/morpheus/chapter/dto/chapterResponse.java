bd7976839dfba26597f807381135cc6e36c48a9a
10Hyuna <kimha105@naver.com>
2024-01-11 04:09:34 +0900

    feat: chapter response dto

---
+++ src/main/java/com/imaginecup/morpheus/chapter/dto/chapterResponse.java
@@ -0,0 +1,23 @@
+package com.imaginecup.morpheus.chapter.dto;
+
+import com.imaginecup.morpheus.picture.domain.Picture;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class chapterResponse {
+
+    private String story;
+
+    private String plot;
+
+    private String background;
+
+    private String narrativeText;
+
+    private int order;
+
+    private Picture image;
+
+}
